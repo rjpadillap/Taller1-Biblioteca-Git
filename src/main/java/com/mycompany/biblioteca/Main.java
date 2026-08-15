b899d768e2c8fc58533f94f1fb9cd3b60482776b
Reinaldo Padilla Pedroza <rjpadilla@unicesar.edu.co>
2026-08-15 07:48:23 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -1,16 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author junio
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static ArrayList<Cliente> clientes = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Aquí irá el menú (Fase 8)
     }
+
+static void crearCliente() {
+    System.out.println("\n--- Crear Cliente ---");
+    System.out.print("ID: ");
+    String id = sc.nextLine();
+    System.out.print("Nombre: ");
+    String nombre = sc.nextLine();
+    System.out.print("Teléfono: ");
+    String telefono = sc.nextLine();
+    System.out.print("Email: ");
+    String email = sc.nextLine();
+
+    Cliente cliente = new Cliente(id, nombre, telefono, email);
+    clientes.add(cliente);
+    System.out.println("Cliente creado exitosamente.");
+}
+
 }
